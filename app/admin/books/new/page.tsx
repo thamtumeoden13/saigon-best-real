06ac6f97ae775ecc9d0c5a2227235e7e8b661a01
VU <vu@VUs-MacBook-Pro-2.local>
2025-03-11 23:06:22 +0700

    Book Form UI

---
+++ app/admin/books/new/page.tsx
@@ -0,0 +1,24 @@
+import BookForm from '@/components/admin/forms/BookForm'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
+
+const Page = () => {
+    return (
+        <>
+            <Button asChild className='back-btn' >
+                <Link href='/admin/books'>
+                    Go Back
+                </Link>
+            </Button>
+
+            <section className='w-full max-w-2xl'>
+                <BookForm 
+                    
+                />
+            </section>
+        </>
+    )
+}
+
+export default Page
